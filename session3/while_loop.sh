0facf509332b5090b26b9f71bb4e638bb2eaf798
Suryaprakash Yadav <suryaprakash.ramkumar@gmail.com>
2022-04-16 21:58:24 +0530

    Added While loop Examples

--- session3/while_loop.sh
+++ session3/while_loop.sh
@@ -6,10 +6,23 @@ while loop
 header
 
 # Example 1
+echo "Printing Example 1"
 VAR1=0 # defined variable with value 0
 
 while [[ "${VAR1}" -lt 10 ]]
 do
 	echo "${VAR1}"
 	((VAR1++))
+done
+echo
+# Example 2
+echo "Printing Example 2"
+ARRAYVAR=(Mahish Abhishek Santosh Bipin Niraj Kamlesh Satish Kundan Dillip)
+
+#Print each element of an Array above in new line
+i=0
+while [[ $i -lt "${#ARRAYVAR[@]}" ]]
+do
+	echo "Element $((i+1)) is ${ARRAYVAR[$i]}"
+	let i=i+1
 done
