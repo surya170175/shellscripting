313848714a486afc0fe9bfbbd1f22e50cb4f515f
Suryaprakash Yadav <suryaprakash.ramkumar@gmail.com>
2022-04-17 11:01:46 +0530

    If then else example updated.

--- session3/if_then_else.sh
+++ session3/if_then_else.sh
@@ -5,14 +5,15 @@
 	if COMMANDS; then COMMANDS; [ elif COMMANDS; then COMMANDS; ]... [ else COMMANDS; ] fi
 ifthenelse
 
-
+echo "Printing for Example 1"
 # Example 1
 # Check if the numper passed as an argument is less than 10
 if [[ "${1}" -lt 10 ]]
 then
 	echo "$1 is less then 10"
 fi
-
+echo
+echo "Printing for Example 2"
 # Example 2
 # Check if user has passed any argument or not
 
@@ -23,6 +24,8 @@ else
 	echo "No. of Arguments passed is: ${#}."
 fi
 
+echo
+echo "Printing for Example 3"
 # Example 3
 # Check if number passed in argument is even or odd
 
@@ -34,10 +37,11 @@ else
 fi
 
 
-# Example 4
 
-echo "Entere name:"
-read n
+# Example 4
+echo "Printing for Example 4"
+
+read -p "Enter Name: " n
 
 if [[ "${n}" = "Bipin" ]]
 then
@@ -48,4 +52,4 @@ then
 else
 	echo "Hello there, how are you?"
 fi
-
+echo
